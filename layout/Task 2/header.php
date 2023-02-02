0e5502412938d3cbc9f60fb7b6ec698c4815d7f5
SHEHAN22000 <shehanmsuv@gmail.com>
2023-02-02 14:37:23 +0200

    Task 2 - Home page linked to the Home tab in the Navbar

--- layout/Task 2/header.php
+++ layout/Task 2/header.php
@@ -25,7 +25,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Home</a>
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="ex1.php">Exercise 1</a>
