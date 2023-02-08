2adf37c1ccdee803d4837d82851d130ac949a3a3
SHEHAN22000 <shehanmsuv@gmail.com>
2023-02-08 19:29:13 +0200

    Exercise 4 - Q1 and Q2

--- layout/controlflow.php
+++ layout/controlflow.php
@@ -2,6 +2,40 @@
 
 <h3> Exercise 4. In-class Task Control flow and loops 08.02.2023 (controlflow.php)</h3>
 
+<h2>1. Write a script that gets the current month and prints one of the following responses...</h2>
+
+<?php
+    $month = date('F');
+    if ($month == 'August'){
+    echo "It's August, so it's still holiday.";
+    }
+    else{
+    echo "Not August, so at least not in the peak of the heat.";
+    }
+?>
+
+<h2> 2. Assign colour red to a variable name $color and check to print one the following responses 
+    (if else statement) </h2>
+
+<?php
+    $color = 'Red';
+    if ($color == 'Red'){
+    echo "The color is red. ";
+    }
+    else{
+    echo "The color is not red.";
+    }
+?>
+
+<h2> 3.  </h2>
+
+
+
+<h2> 4.  </h2>
+
+
+<h2> 5.  </h2>
+
 
 
 
