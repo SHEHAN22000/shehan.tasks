62e023247e46d5e92fe9fa8f91d1436812885c96
SHEHAN22000 <shehanmsuv@gmail.com>
2023-02-08 20:39:05 +0200

    Exercise 4 completed

--- layout/controlflow.php
+++ layout/controlflow.php
@@ -27,17 +27,80 @@
     }
 ?>
 
-<h2> 3.  </h2>
+<h2> 3. Write a program to grade students based on their total score for a subject. </h2>
+
+<?php
+    $score = 100;
+    if ($score > 80)
+    {
+    echo "Excellent ";
+    }
+    elseif ($score > 70) 
+    {
+    echo "Great ";
+    }
+    elseif ($score > 60) 
+    {
+    echo "Good ";
+    }  
+    elseif ($score > 50) 
+    {
+    echo "Pass ";
+    }  
+    elseif ($score < 50) 
+    {
+    echo "Fail ";
+    }
+?>
+
+<h2> 4. Write a program to get inputs (age and name) from the user and based on their age, 
+    decide if he/she is eligible for voting. </h2>
+
+<?php
+    function check_vote() {
+    $name = "Name";
+    $age = 00;
+    if ($age >= 18) {
+        echo $name . ", You Are Eligible For Vote";
+    } else {
+        echo $name . ", You are not eligible for vote. ";
+    }
+    }
+    check_vote(); 
+?>
+
+<h2> 5. If someone is using Chrome it should print, you are using Goolge Chrome….</h2>
+
+<?php
+
+    function getBrowser()
+    {
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $browser = "N/A";
+
+    $browsers = [
+     '/msie/i' => 'Internet explorer',
+     '/firefox/i' => 'Firefox',
+     '/safari/i' => 'Safari',
+     '/chrome/i' => 'Chrome',
+     '/edge/i' => 'Edge',
+     '/opera/i' => 'Opera',
+     '/mobile/i' => 'Mobile browser',
+    ];
 
 
+   foreach ($browsers as $regex => $value) 
+    {
+     if (preg_match($regex, $user_agent)) 
+    {
+       $browser = $value;
+     }
+    }
+    return $browser;
+}
 
-<h2> 4.  </h2>
+    echo "You are using: " . getBrowser();
 
-
-<h2> 5.  </h2>
-
-
-
-
+?>
 
 <?php include "footer.php" ?>
