c3b89c14b6109fae76ea73b4d57f2f76f1f9a616
SHEHAN22000 <shehanmsuv@gmail.com>
2023-02-14 09:09:13 +0200

    read.php

---
+++ crud1/read.php
@@ -0,0 +1,17 @@
+<?php include "header.php" ?>
+
+
+<?php
+    $title = "Reading Data from the database";
+    include '../layout/header.php';
+    include 'db.php';
+    $sql = "select * from studentinfo";
+
+?>
+
+
+
+
+
+
+<?php include "footer.php" ?>
